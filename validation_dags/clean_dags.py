1099f4e67b8eb18084cfd793497ba75749589438
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-06-22 11:34:43 -0400

    .

--- validation_dags/clean_dags.py
+++ validation_dags/clean_dags.py
@@ -3,9 +3,6 @@ from airflow_config import Dag, load_config
 
 config = load_config("config", "config")
 
-with Dag(
-    dag_id="lam-clean-dags-noconfig",
-    params=DagCleanupTask,
-).instantiate(config=config) as dag:
+with Dag(dag_id="lam-clean-dags-noconfig", params=DagCleanupTask).instantiate(config=config) as dag:
     task = DagCleanupTask(task_id="lam-cleanup-dag-runs")
     task.instantiate(dag=dag)
