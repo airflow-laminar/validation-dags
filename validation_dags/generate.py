27f7e1b564da76f3514a9aaeb17ddaaac1a0e373
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-07-03 16:09:08 -0400

    .

--- validation_dags/generate.py
+++ validation_dags/generate.py
@@ -1,4 +1,4 @@
-# from airflow_config import load_config
+from airflow_config import load_config
 
-# config = load_config("config", "config")
-# config.generate_in_mem(placeholder_dag_id="lam-generate-dags")
+config = load_config("config", "config")
+config.generate_in_mem(placeholder_dag_id="lam-generate-dags")
