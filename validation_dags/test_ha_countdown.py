a486bb440a31e6dac7fafe202d0427a03e0a501b
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-07-03 17:18:57 -0400

    .

--- validation_dags/test_ha_countdown.py
+++ validation_dags/test_ha_countdown.py
@@ -11,13 +11,13 @@ with DAG(
     dag_id="lam-test-ha-counter-noconfig",
     config=config,
 ) as dag:
-    get_count = PythonOperator(task_id="lam_get_count", python_callable=_get_count, do_xcom_push=True)
+    get_count = PythonOperator(task_id="lam-get-count", python_callable=_get_count, do_xcom_push=True)
     keep_counting = HighAvailabilityOperator(
         task_id="lam-ha",
         timeout=30,
         poke_interval=5,
         python_callable=_keep_counting,
-        pass_trigger_kwargs={"conf": '{"counter": {{ ti.xcom_pull(key="return_value", task_ids="lam_get_count") }} }'},
+        pass_trigger_kwargs={"conf": '{"counter": {{ ti.xcom_pull(key="return_value", task_ids="lam-get-count") }} }'},
     )
 
     get_count >> keep_counting
